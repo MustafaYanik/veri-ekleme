a79da7f3e557647e5e3c652514f08a1354a04394
MustafaYanik <91061604+MustafaYanik@users.noreply.github.com>
2022-03-18 12:11:15 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtad = new System.Windows.Forms.TextBox();
+            this.txtsoyad = new System.Windows.Forms.TextBox();
+            this.txtkardessayisi = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.rdberkek = new System.Windows.Forms.RadioButton();
+            this.rdbkadın = new System.Windows.Forms.RadioButton();
+            this.txtortalama = new System.Windows.Forms.TextBox();
+            this.txtaylikgeilir = new System.Windows.Forms.TextBox();
+            this.cmbegitim = new System.Windows.Forms.ComboBox();
+            this.chdyaptınmı = new System.Windows.Forms.CheckBox();
+            this.btnkaydet = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SOYAD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "DOĞUM TARİHİ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "KARDEŞ SAYISI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(87, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "CİNSİYET";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(405, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "NOT ORTALAMASI";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(432, 59);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "AYLIK GELİRİ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(411, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "EĞİTİM DURUMU";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(327, 111);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(180, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "DAHA ÖNCE BAŞVURU YAPTIMI ?";
+            // 
+            // txtad
+            // 
+            this.txtad.Location = new System.Drawing.Point(140, 26);
+            this.txtad.Name = "txtad";
+            this.txtad.Size = new System.Drawing.Size(100, 20);
+            this.txtad.TabIndex = 9;
+            // 
+            // txtsoyad
+            // 
+            this.txtsoyad.Location = new System.Drawing.Point(140, 58);
+            this.txtsoyad.Name = "txtsoyad";
+            this.txtsoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtsoyad.TabIndex = 10;
+            // 
+            // txtkardessayisi
+            // 
+            this.txtkardessayisi.Location = new System.Drawing.Point(140, 122);
+            this.txtkardessayisi.Name = "txtkardessayisi";
+            this.txtkardessayisi.Size = new System.Drawing.Size(100, 20);
+            this.txtkardessayisi.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(140, 88);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(100, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // rdberkek
+            // 
+            this.rdberkek.AutoSize = true;
+            this.rdberkek.Location = new System.Drawing.Point(149, 155);
+            this.rdberkek.Name = "rdberkek";
+            this.rdberkek.Size = new System.Drawing.Size(61, 17);
+            this.rdberkek.TabIndex = 13;
+            this.rdberkek.TabStop = true;
+            this.rdberkek.Text = "ERKEK";
+            this.rdberkek.UseVisualStyleBackColor = true;
+            // 
+            // rdbkadın
+            // 
+            this.rdbkadın.AutoSize = true;
+            this.rdbkadın.Location = new System.Drawing.Point(216, 155);
+            this.rdbkadın.Name = "rdbkadın";
+            this.rdbkadın.Size = new System.Drawing.Size(58, 17);
+            this.rdbkadın.TabIndex = 14;
+            this.rdbkadın.TabStop = true;
+            this.rdbkadın.Text = "KADIN";
+            this.rdbkadın.UseVisualStyleBackColor = true;
+            
+            // 
+            // txtortalama
+            // 
+            this.txtortalama.Location = new System.Drawing.Point(513, 30);
+            this.txtortalama.Name = "txtortalama";
+            this.txtortalama.Size = new System.Drawing.Size(100, 20);
+            this.txtortalama.TabIndex = 15;
+            // 
+            // txtaylikgeilir
+            // 
+            this.txtaylikgeilir.Location = new System.Drawing.Point(513, 56);
+            this.txtaylikgeilir.Name = "txtaylikgeilir";
+            this.txtaylikgeilir.Size = new System.Drawing.Size(100, 20);
+            this.txtaylikgeilir.TabIndex = 16;
+            // 
+            // cmbegitim
+            // 
+            this.cmbegitim.FormattingEnabled = true;
+            this.cmbegitim.Items.AddRange(new object[] {
+            "İLKOKUL",
+            "ORTAOKUL",
+            "LİSE",
+            "LİSANS",
+            "YÜKSEK LİSANS"});
+            this.cmbegitim.Location = new System.Drawing.Point(513, 82);
+            this.cmbegitim.Name = "cmbegitim";
+            this.cmbegitim.Size = new System.Drawing.Size(100, 21);
+            this.cmbegitim.TabIndex = 17;
+            // 
+            // chdyaptınmı
+            // 
+            this.chdyaptınmı.AutoSize = true;
+            this.chdyaptınmı.Location = new System.Drawing.Point(513, 111);
+            this.chdyaptınmı.Name = "chdyaptınmı";
+            this.chdyaptınmı.Size = new System.Drawing.Size(15, 14);
+            this.chdyaptınmı.TabIndex = 18;
+            this.chdyaptınmı.UseVisualStyleBackColor = true;
+            // 
+            // btnkaydet
+            // 
+            this.btnkaydet.Location = new System.Drawing.Point(451, 144);
+            this.btnkaydet.Name = "btnkaydet";
+            this.btnkaydet.Size = new System.Drawing.Size(102, 26);
+            this.btnkaydet.TabIndex = 19;
+            this.btnkaydet.Text = "KAYDET";
+            this.btnkaydet.UseVisualStyleBackColor = true;
+            this.btnkaydet.Click += new System.EventHandler(this.btnkaydet_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 207);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(640, 257);
+            this.dataGridView1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 476);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnkaydet);
+            this.Controls.Add(this.chdyaptınmı);
+            this.Controls.Add(this.cmbegitim);
+            this.Controls.Add(this.txtaylikgeilir);
+            this.Controls.Add(this.txtortalama);
+            this.Controls.Add(this.rdbkadın);
+            this.Controls.Add(this.rdberkek);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtkardessayisi);
+            this.Controls.Add(this.txtsoyad);
+            this.Controls.Add(this.txtad);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtad;
+        private System.Windows.Forms.TextBox txtsoyad;
+        private System.Windows.Forms.TextBox txtkardessayisi;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.RadioButton rdberkek;
+        private System.Windows.Forms.RadioButton rdbkadın;
+        private System.Windows.Forms.TextBox txtortalama;
+        private System.Windows.Forms.TextBox txtaylikgeilir;
+        private System.Windows.Forms.ComboBox cmbegitim;
+        private System.Windows.Forms.CheckBox chdyaptınmı;
+        private System.Windows.Forms.Button btnkaydet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection connection;
+        OleDbDataAdapter adapter;
+        OleDbCommand command;
+        DataTable table;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            doldur();
+        }
+            private void doldur()
+            {
+               connection = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0;Data Source=vt1.accdb");
+               adapter = new OleDbDataAdapter("select * from bilgiler",connection);
+               connection.Open();
+               table = new DataTable();
+               adapter.Fill(table);
+               dataGridView1.DataSource = table;
+               connection.Close();
+            }
+        private void btnkaydet_Click(object sender, EventArgs e)
+        {
+            command = new OleDbCommand();
+            connection.Open();
+            command.Connection = connection;
+            command.CommandText = " insert into bilgiler (ad,soyad,doğumtarihi,kardeşsayısı,cinsiyet,notortalaması,aylıkgeliri,eğitimdurumu,dahaöncebaşvuruyaptınmı) values (@ad,@soyad,@doğumtarihi,@kardeşsayısı,@cinsiyet,@notortalaması,@aylıkgeliri,@eğitimdurumu,@dahaöncebaşvuruyaptınmı)";
+
+            command.Parameters.AddWithValue("@ad",txtad.Text);
+            command.Parameters.AddWithValue("@soyad", txtsoyad.Text);
+            command.Parameters.AddWithValue("@doğumtarihi", dateTimePicker1.Value.ToShortDateString());
+            command.Parameters.AddWithValue("@kardeşsayısı", int.Parse(txtkardessayisi.Text));
+
+            if (rdberkek.Checked)
+                command.Parameters.AddWithValue("@cinsiyet", rdberkek.Text);
+            if (rdbkadın.Checked)
+                command.Parameters.AddWithValue("@cinsiyet", rdbkadın.Text);
+
+            command.Parameters.AddWithValue("@notortalaması", double.Parse(txtortalama.Text));
+            command.Parameters.AddWithValue("@aylıkgeliri", decimal.Parse(txtaylikgeilir.Text));
+            command.Parameters.AddWithValue("@eğitimdurumu", cmbegitim.SelectedItem.ToString());
+
+            if (chdyaptınmı.Checked)
+                command.Parameters.AddWithValue("@dahaöncebaşvuruyaptınmı", true);
+            else
+                command.Parameters.AddWithValue("@dahaöncebaşvuruyaptınmı", false);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+            doldur();
+        }
+    }
+}
